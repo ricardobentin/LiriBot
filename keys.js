5cdf53bc88d28980e67d9f1d2d179f00b8804d46
Nano Bacano <ricardo.bentin@gmail.com>
2018-08-06 14:51:35 -0400

    twitter is working. Going to work on Spotify

--- keys.js
+++ keys.js
@@ -7,7 +7,8 @@ exports.twitter = {
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
--- liri.js
+++ liri.js
@@ -1,1 +1,26 @@
-var test2;
+//to read and set any environment variables with the dotenv package
+require("dotenv").config();
+var request = require("request");
+//importing the keys
+var keys = require("./keys.js");
+var Twitter = require('twitter')
+var Spotify = require('node-spotify-api');
+
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+
+switch (process.argv[2]) {
+    case "my-tweets":
+        var params = { screen_name: 'ricardobentin' };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error && response.statusCode === 200) {
+                for (var i = 0; i < 20; i++) {
+                    console.log(`Tweet Number. ${i}: ${tweets[i].text}`);
+                }
+            }
+        });
+        break;
+    default:
+        console.log(`You ran a case you haven't configured yet`);
+}
+
